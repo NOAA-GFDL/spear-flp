5ff9bec4a2f07abc9e0b71f29a93556ee0612688
Aria Radick <--set>
2025-07-16 13:30:01 -0400

    remove prefix on file lists

--- data_transfer/get_file_lists.py
+++ data_transfer/get_file_lists.py
@@ -1,4 +1,4 @@
-import intake,os
+import intake,sys
 import intake_esm
 import pandas as pd
 
@@ -7,13 +7,20 @@ def main(catalog_path):
     all_variables = set(cat.df['variable_id'])
     afiles = []
     dfiles = []
+
     for x in all_variables:
-        d_df = cat.df.where((cat.df['variable_id'] == x) & (cat.df['path'].str.contains("decp")))
-        a_df = cat.df.where((cat.df['variable_id'] == x) & (cat.df['path'].str.contains("archive")))
-        if d_df.shape[0] == a_df.shape[0]:
+        d_df = cat.df[(cat.df['variable_id'] == x) & (cat.df['path'].str.contains("decp"))]
+        a_df = cat.df[(cat.df['variable_id'] == x) & (cat.df['path'].str.contains("archive"))]
+        if d_df.shape[0] > 0:
             dfiles.extend(d_df.path.to_list())
         else:
             afiles.extend(a_df.path.to_list())
+
+    for i,x in enumerate(afiles):
+        afiles[i] = x.replace("/archive/wfc/SPEAR/", "")
+    for i,x in enumerate(dfiles):
+        dfiles[i] = x.replace("/decp/SPEAR_MED/", "")
+
     pd.Series(afiles).to_csv("archive_files.txt", header=None, index=False)
     pd.Series(dfiles).to_csv("decp_files.txt", header=None, index=False)
 
